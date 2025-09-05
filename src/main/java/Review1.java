b1f6e37534e9f3ab32f579eacaa23b285f076c02
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-09-05 23:48:20 +0000

    Committing new changes

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,10 +2,15 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.")
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int nine = 9;
+		int five = 5;
+		System.out.println(nine / five);
+		System.out.println(nine % five);
 	}
 
 	public static void q3() {
